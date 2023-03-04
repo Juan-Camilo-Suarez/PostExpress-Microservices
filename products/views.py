d80f925fce0dea43963253f4e8d60f94f10fd85b
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-04 12:38:37 +0300

    +++ create architecture for ProductViewSet

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
 
 # Create your views here.
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        pass
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def delete(self, request, pk=None):
+        pass
