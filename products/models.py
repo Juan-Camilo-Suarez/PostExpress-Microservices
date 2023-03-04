39357a57634387f8a42a5483a51af272ab5bb038
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-04 12:29:42 +0300

    +++ create models in the product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=150)
+    image = models.CharField(max_length=200)
+    description = models.TextField(max_length=250)
+    likes = models.PositiveIntegerField(default=0)
+
+
+"""
+with this model we will get only the id of user 
+"""
+
+
+class User(models.Model):
+    pass
