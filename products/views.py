0a91cd0cdd2bcea5b5997ccce64dc3d247e84b5c
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-04 13:11:01 +0300

    +++ function create of products in ProductViewSet

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from products.models import Product
@@ -14,7 +14,10 @@ class ProductViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
     def create(self, request):
-        pass
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
         pass
