d13b85bdb78d280606c39029e4005fdc3a34effd
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-05 11:46:10 +0300

    +++ flask connection with db

--- MainFlask/main.py
+++ MainFlask/main.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://your_username:your_password@db/main'
+CORS(app)
+
+db = SQLAlchemy(app)
 
 
 @app.route("/")
