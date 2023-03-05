83f0c215c5feb87b1e701f5aaabc5b5e79efca71
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-05 12:07:39 +0300

    +++ created models into the flask main

--- MainFlask/main.py
+++ MainFlask/main.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -10,9 +12,26 @@ CORS(app)
 db = SQLAlchemy(app)
 
 
-@app.route("/")
-def index():
-    return "hello"
+@dataclass
+class Product(db.Model):
+    id: int
+    title: str
+    description: str
+    image: str
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title = db.Column(db.String(200))
+    description = db.Column(db.String(300))
+    image = db.Column(db.String(200))
+
+
+@dataclass
+class ProductUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+
+    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 
 if __name__ == "__main__":
