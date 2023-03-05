62523a6c74a449cfdfda5bc1519f845e8f1f05d3
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-05 22:08:42 +0300

    +++ added auto_ack=True in costumers

--- DjangoAdmin/consumer.py
+++ DjangoAdmin/consumer.py
@@ -13,7 +13,7 @@ def callback(ch, method, properties, body):
     print(body)
 
 
-channel.basic_consume(queue='admin', on_message_callback=callback)
+channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
 
 print('started Consuming')
 
