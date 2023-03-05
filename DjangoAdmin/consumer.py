ef4435e3f5caa7b5465e89e7d5e3fec74a4ca239
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-05 15:23:38 +0300

    +++ crete a consumer with rabbitmq

---
+++ DjangoAdmin/consumer.py
@@ -0,0 +1,22 @@
+import pika
+
+params = pika.URLParameters('amqps://mjwdfmdv:1IvG-RJ-ZLYLN92M9HPWPZNwrCuSO1RS@rat.rmq2.cloudamqp.com/mjwdfmdv')
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='admin')
+
+
+def callback(ch, method, properties, body):
+    print('received in admin')
+    print(body)
+
+
+channel.basic_consume(queue='admin', on_message_callback=callback)
+
+print('started Consuming')
+
+channel.start_consuming()
+
+channel.close()
