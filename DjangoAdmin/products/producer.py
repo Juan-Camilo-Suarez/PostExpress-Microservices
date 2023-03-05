80237f530e253c5a0cf19097a100c2f169dfea5d
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-05 15:23:07 +0300

    +++ crete a producer with rabbitmq

---
+++ DjangoAdmin/products/producer.py
@@ -0,0 +1,10 @@
+import pika
+
+params = pika.URLParameters('amqps://mjwdfmdv:1IvG-RJ-ZLYLN92M9HPWPZNwrCuSO1RS@rat.rmq2.cloudamqp.com/mjwdfmdv')
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+def publish():
+    channel.basic_publish(exchange='', routing_key='admin', body='Hello')
