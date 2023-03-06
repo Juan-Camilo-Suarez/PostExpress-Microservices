84c3739cf1509a115ca02b40f40333801969b5d5
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-06 08:30:39 +0300

    +++ completed producer

--- DjangoAdmin/products/producer.py
+++ DjangoAdmin/products/producer.py
@@ -1,4 +1,4 @@
-import pika
+import pika, json
 
 params = pika.URLParameters('amqps://mjwdfmdv:1IvG-RJ-ZLYLN92M9HPWPZNwrCuSO1RS@rat.rmq2.cloudamqp.com/mjwdfmdv')
 connection = pika.BlockingConnection(params)
@@ -6,5 +6,6 @@ connection = pika.BlockingConnection(params)
 channel = connection.channel()
 
 
-def publish():
-    channel.basic_publish(exchange='', routing_key='main', body='Hello main')
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
