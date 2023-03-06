e93032601560170077fbfab46dea9144eb83f9f4
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-06 21:51:43 +0300

    +++ corrected producer

--- DjangoAdmin/products/producer.py
+++ DjangoAdmin/products/producer.py
@@ -1,4 +1,6 @@
-import pika, json
+import json
+
+import pika
 
 params = pika.URLParameters('amqps://mjwdfmdv:1IvG-RJ-ZLYLN92M9HPWPZNwrCuSO1RS@rat.rmq2.cloudamqp.com/mjwdfmdv')
 connection = pika.BlockingConnection(params)
