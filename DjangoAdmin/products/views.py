69061331fd2cd6a25562fe4762eddbc4e376531f
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-06 08:34:29 +0300

    +++ added methods to producer in view

--- DjangoAdmin/products/views.py
+++ DjangoAdmin/products/views.py
@@ -15,13 +15,13 @@ class ProductViewSet(viewsets.ViewSet):
     def list(self, request):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
-        publish()
         return Response(serializer.data)
 
     def create(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        publish('product_created', serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
@@ -34,11 +34,13 @@ class ProductViewSet(viewsets.ViewSet):
         serializer = ProductSerializer(instance=products, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        publish('product_updated', serializer.data)
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def destroy(self, request, pk=None):
         products = Product.objects.get(id=pk)
         products.delete()
+        publish('product_deleted', pk)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
