e9df629d7a42e7515a3f7aa60a917653b5cbf8d6
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-07 15:31:36 +0300

    +++ create view to get list products(posts)

--- MainFlask/main.py
+++ MainFlask/main.py
@@ -1,6 +1,36 @@
-from flask import Flask
+from dataclasses import dataclass
+
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# initialize SQLAlchemy with app instance
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://your_username:your_password@db/main'
+db = SQLAlchemy(app)
+
+
+@dataclass
+class Product(db.Model):
+    id: int
+    title: str
+    description: str
+    image: str
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title = db.Column(db.String(200))
+    description = db.Column(db.String(300))
+    image = db.Column(db.String(200))
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/api/products")
+def index():
+    return jsonify(Product.query.all())
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
