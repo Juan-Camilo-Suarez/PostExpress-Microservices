ca102caed03dd9a5ab32ef96e021ea079c56062a
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-07 23:36:09 +0300

    +++ create a model Productuser with like

--- MainFlask/main.py
+++ MainFlask/main.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass
 
 import requests
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 
-from app import Product, app
+from app import Product, app, ProductUser, db
 
 
 @app.route("/")
@@ -16,12 +16,24 @@ def hello():
 def index():
     return jsonify(Product.query.all())
 
+
 @app.route("/api/products/<int:id>/like", methods=["POST"])
 def like(id):
     req = requests.get('http://djangoadmin-backend-1:8000/api/user')
-    return jsonify(req.json())
+    json = req.json()
+    try:
+        productUser = ProductUser(user_id=json['id'], product_id=id)
+        db.session.add(productUser)
+        db.session.commit()
+
+        # publish('product_liked', id)
+    except:
+        abort(400, 'You already liked this product')
+
+    return jsonify({
+        'message': 'success'
+    })
 
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
-
