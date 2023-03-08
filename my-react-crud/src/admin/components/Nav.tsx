b1c77d5a14f5285745dbf781da305b5945cc2448
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-08 19:22:40 +0300

    +++ change name of site

--- my-react-crud/src/admin/components/Nav.tsx
+++ my-react-crud/src/admin/components/Nav.tsx
@@ -4,7 +4,7 @@ const Nav = () => {
     return (
 
         <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-            <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="my-react-crud/src/admin/components#">Company name</a>
+            <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="my-react-crud/src/admin/components#">Post Express</a>
             <button className="navbar-toggler position-absolute d-md-none collapsed" type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
