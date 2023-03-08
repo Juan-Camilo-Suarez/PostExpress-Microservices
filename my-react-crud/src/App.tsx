6ccd2bebd8b41db16711a51fdc3ede43ed59cbf5
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-08 12:51:30 +0300

    +++ created routes in app

--- my-react-crud/src/App.tsx
+++ my-react-crud/src/App.tsx
@@ -1,33 +1,23 @@
 import React from 'react';
 import './App.css';
-import Nav from "./components/Nav";
-import Menu from "./components/Menu";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Products from "./admin/Products";
+import Main from "./main/Main";
 
 function App() {
     return (
 
         <div className="App">
-            <Nav/>
 
 
-            <div className="container-fluid">
-                <div className="row">
-                    <Menu/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/'  element={<Main/>}/>
+                    <Route path='/admin/products' element={<Products/>}/>
 
-                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                        <BrowserRouter>
-                            {/*<Route path='/admin/products' component={Products}/>*/}
-                            <Route path='/admin/products'>
-                                <Products/>
-                            </Route>
-                        </BrowserRouter>
+                </Routes>
+            </BrowserRouter>
 
-
-                    </main>
-                </div>
-            </div>
 
         </div>
     );
