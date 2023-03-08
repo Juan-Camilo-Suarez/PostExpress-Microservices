6ba991b31d3b841fced8d8f62f07f798f8aae3fb
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-08 10:51:59 +0300

    +++ Created Products component

--- my-react-crud/src/App.tsx
+++ my-react-crud/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Nav from "./components/Nav";
 import Menu from "./components/Menu";
+import Products from "./admin/Products";
 
 function App() {
     return (
@@ -15,30 +16,10 @@ function App() {
                     <Menu/>
 
                     <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                        <Products/>
 
-                        <h2>Section title</h2>
-                        <div className="table-responsive">
-                            <table className="table table-striped table-sm">
-                                <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Header</th>
-                                    <th scope="col">Header</th>
-                                    <th scope="col">Header</th>
-                                    <th scope="col">Header</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td>1,001</td>
-                                    <td>random</td>
-                                    <td>data</td>
-                                    <td>placeholder</td>
-                                    <td>text</td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
+
+
                     </main>
                 </div>
             </div>
---
+++ my-react-crud/src/admin/Products.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <div className="table-responsive">
+                <table className="table table-striped table-sm">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Header</th>
+                        <th scope="col">Header</th>
+                        <th scope="col">Header</th>
+                        <th scope="col">Header</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1,001</td>
+                        <td>random</td>
+                        <td>data</td>
+                        <td>placeholder</td>
+                        <td>text</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    );
+};
+
+export default Products;
+
