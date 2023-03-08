68522e689bd78ab83a8575854f93166428474a2a
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-08 16:02:12 +0300

    +++ component products with functions to get posts from api and render in table

--- my-react-crud/src/admin/Products.tsx
+++ my-react-crud/src/admin/Products.tsx
@@ -1,34 +1,68 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Wrapper from "./Wrapper";
+import {Link} from "react-router-dom";
+import {Product} from "./interfaces/product";
 
 const Products = () => {
+
+    const [products, setProducts] = useState([]);
+
+
+    // to get products from api
+    useEffect(() => {
+        (
+            async () => {
+                const response = await fetch('http://localhost:8000/api/products');
+
+                const data = await response.json();
+
+                console.log(data);
+                setProducts(data);
+            }
+        )()
+    }, [])
     return (
         <Wrapper>
             <div>
-            <div className="table-responsive">
-                <table className="table table-striped table-sm">
-                    <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1,001</td>
-                        <td>random</td>
-                        <td>data</td>
-                        <td>placeholder</td>
-                        <td>text</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
+                <div className="table-responsive">
+                    <table className="table table-striped table-sm">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Image</th>
+                            <th>Title</th>
+                            <th>Description</th>
+                            <th>Likes</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {products.map((p: Product) => {
+                            return (
+                                <tr key={p.id}>
+                                    <td>{p.id}</td>
+                                    <td><img src={p.image} height="180"/></td>
+                                    <td>{p.title}</td>
+                                    <td>{p.description}</td>
+                                    <td>{p.likes}</td>
+                                    {/*<td>*/}
+                                    {/*    <div className="btn-group mr-2">*/}
+                                    {/*        <Link to={`/admin/products/${p.id}/edit`}*/}
+                                    {/*              className="btn btn-sm btn-outline-secondary">Edit</Link>*/}
+                                    {/*        <a href="#" className="btn btn-sm btn-outline-secondary"*/}
+                                    {/*           onClick={() => del(p.id)}*/}
+                                    {/*        >Delete</a>*/}
+                                    {/*    </div>*/}
+                                    {/*</td>*/}
+                                </tr>
+                            )
+                        })}
 
-        </div>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
         </Wrapper>
 
     );
