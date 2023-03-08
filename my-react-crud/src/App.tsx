aba3157cc4eda64892b465091c1703bea9d07484
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-08 10:32:00 +0300

    +++ added bootstrap dashboard

--- my-react-crud/src/App.tsx
+++ my-react-crud/src/App.tsx
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+
+        <div className="App">
+            <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+                <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Company name</a>
+                <button className="navbar-toggler position-absolute d-md-none collapsed" type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <input className="form-control form-control-dark w-100 rounded-0 border-0"
+                       type="text" placeholder="Search" aria-label="Search"/>
+                <div className="navbar-nav">
+                    <div className="nav-item text-nowrap">
+                        <a className="nav-link px-3" href="#">Sign out</a>
+                    </div>
+                </div>
+            </header>
+
+            <div className="container-fluid">
+                <div className="row">
+                    <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                        <div className="position-sticky pt-3 sidebar-sticky">
+                            <ul className="nav flex-column">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#">
+                                        <span data-feather="home" className="align-text-bottom"></span>
+                                        Dashboard
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+
+                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
+                        <h2>Section title</h2>
+                        <div className="table-responsive">
+                            <table className="table table-striped table-sm">
+                                <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Header</th>
+                                    <th scope="col">Header</th>
+                                    <th scope="col">Header</th>
+                                    <th scope="col">Header</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>1,001</td>
+                                    <td>random</td>
+                                    <td>data</td>
+                                    <td>placeholder</td>
+                                    <td>text</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </main>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
